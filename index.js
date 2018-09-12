63a5cb9dc6f048cfaa51fb0786519d5fe48cdc98
Samuel Prada <jspradac@gmail.com>
2018-09-12 18:26:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+function getFirstSelector(selector) {
+return document.querySelector(selector);  
+}
+function nestedTarget(target, nested) {
+  return document.querySelector('#nested .target');
+}
+function increaseRankBy(n) {
+  const rank = document.querySelectorAll(".ranked-list li");
+  for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+}
+function deepestChild() {
+  const deep = document.querySelectorAll("#grand-node div");
+  return deep[deep.length - 1];
+}
